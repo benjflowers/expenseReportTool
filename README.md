8627e9f38d3bc178edd353c841290197ecb59e37
benjflowers <benjaminjflowers@gmail.com>
2021-08-27 12:46:31 -0500

    Returning month with expense

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Using Ruby to parse CSV and generate a report
 
-# if table.by_col[0][0].split(" ")[0] == "Account" it is a bank statementl
-# If a CACU statement column mappings are as follows
-# Description Column
-# table.by_col[2]
+if table.by_col[0][0].split(" ")[0] == "Account" it is a bank statement
 
-# Date Column
-# table.by_col[1]
+## If a CACU statement column mappings are as follows
 
-# Amount Column
-# table.by_col[4]
+Description Column
+table.by_col[2]
 
-# If a Credit Statement column mappings are as follows
-# Description Column
-# table.by_col[2]
+Date Column
+table.by_col[1]
 
-# Amount Column 
-# table.by_col[5]
+Amount Column
+table.by_col[4]
 
-# Date Column
-# table.by_col[0]
+If a Credit Statement column mappings are as follows
+Description Column
+table.by_col[2]
+
+Amount Column 
+table.by_col[5]
+
+Date Column
+table.by_col[0]
--- main.rb
+++ main.rb
@@ -1,22 +1,41 @@
 require 'csv'
 require 'pp'
+require 'date'
 
 statements = Dir["./statements/*"]
 history = []
 table = CSV.parse(File.read(statements[2]), headers: true)
 
+# Inserts expense objects...
+# expense_object: [{ @description: @amount }...]
 statements.each do |statement|
   table = CSV.parse(File.read(statement), headers: true)
-  entry = {}
+
   if table.by_col[0][0].split(" ")[0] == "Account"
     table.by_col[2][3..-1].each_with_index do |description, index|
-      history.push({"#{description}": table.by_col[4][3..-1][index]})
+      formatted_date = table.by_col[1][3..-1][index].split("/")
+      date = Date.new(
+        formatted_date[2].to_i,
+        formatted_date[0].to_i,
+        formatted_date[1].to_i
+      )
+      month = date.strftime("%b")
+      entry = { "#{month}": {"#{description}": table.by_col[4][3..-1][index]} }
+      history.push(entry)
     end
   else
     table.by_col[2].each_with_index do |description, index|
-      history.push({"#{description}": table.by_col[5][index]})
+      formatted_date = table.by_col[1][3..-1][index].split("/")
+      date = Date.new(
+        formatted_date[2].to_i,
+        formatted_date[0].to_i,
+        formatted_date[1].to_i
+      )
+      month = date.strftime("%b")
+      entry = { "#{month}": {"#{description}": table.by_col[5][3..-1][index]} }
+      history.push(entry)
     end
   end
 end
 
-print history
+puts history
