03e0a213ff319287539b4d4586d48028c13b7d70
benjflowers <benjaminjflowers@gmail.com>
2021-08-28 18:59:27 -0500

    Returning array of months with nested expenses that were made in each month

--- main.rb
+++ main.rb
@@ -1,61 +1,128 @@
 require 'csv'
 require 'pp'
 require 'date'
+require 'pry'
 
 statements = Dir["./statements/*"]
 history = []
 table = CSV.parse(File.read(statements[2]), headers: true)
 
-# Inserts expense objects...
-# expense_object: [{ @description: @amount }...]
-statements.each do |statement|
-  table = CSV.parse(File.read(statement), headers: true)
+# V2 ---------
+# Final return
+expense_summary = []
 
-  if table.by_col[0][0].split(" ")[0] == "Account"
-    table.by_col[2][3..-1].each_with_index do |description, index|
-      formatted_date = table.by_col[1][3..-1][index].split("/")
-      date = Date.new(
-        formatted_date[2].to_i,
-        formatted_date[0].to_i,
-        formatted_date[1].to_i
-      )
-      month = date.strftime("%b")
-      entry = { "#{month}": {"#{description}": table.by_col[4][3..-1][index]} }
-      history.push(entry)
+# Returns the string value of a month given its integer
+#
+# @return: String - "Jul"
+# @params: date - Array - ["06", "24", "2021"]
+def month_in(date)
+  # Date.new requires Y/M/D
+  Date.new(
+    date.split("/")[2].to_i,
+    date.split("/")[0].to_i,
+    date.split("/")[1].to_i
+  ).strftime("%b")
+end
+
+# Returns an array of the months included in the statement
+#
+# @returns: Array - ["Jul", "Jun"]
+# @params: date_column - Array - ["06/24/2021"]
+def collected_months_in_column(date_column)
+  collection = []
+  collection.push(month_in(date_column[0]))
+  collection.push(month_in(date_column[-1]))
+
+  collection
+end
+
+# Insert the Month hash into the final summary array
+#
+# @returns: nil
+# @params: expense_summary - Array - []
+# @params: satements - CSV - Object
+def insert_months_into_summary(statements, expense_summary = [])
+  all_months = []
+  statements.each do |statement|
+    # entire table as an object
+    table = CSV.parse(File.read(statement), headers: true)
+
+    # Date column in table: Array []
+    date_column = table.by_col[1][3..-1]
+
+    month_in(date_column[0])
+    month_in(date_column[-1])
+
+    all_months.push(collected_months_in_column(date_column))
+  end
+
+  all_months.flatten.uniq.each do |month|
+    expense_summary.push({"#{month}": []})
+  end
+end
+
+# Extract the description and amounts from each row
+#
+# @returns: Array [{description: amount}]
+# @params: table - Array - []
+# @params: desc_index - Int - 2
+# @params: amount_index - Int - 4
+def extracted_table_row_data(table: [], date_index: 0, desc_index: 0, amount_index: 0)
+  entries = []
+  table.each do |row|
+    if row[amount_index] == nil
+      row[amount_index] = "0"
     end
+    entries.push({
+      "#{month_in(row[date_index])}": {
+        "#{row[desc_index]}": row[amount_index]
+      }
+    })
+  end
+
+  entries
+end
+
+insert_months_into_summary(statements, expense_summary)
+
+statements.each do |statement|
+  data = []
+  table = CSV.parse(File.read(statement), headers: true)
+  # if bank CACU statement we have to alter
+  # where we access rows
+  if table[0][0].include?("Account")
+    # entry_index = 3
+    # date_index = 1
+    # desc_index = 2
+    # amount_index = 4
+
+    data = extracted_table_row_data(
+      table: table[3..-1],
+      date_index: 1,
+      desc_index: 2,
+      amount_index: 4
+    )
   else
-    table.by_col[2].each_with_index do |description, index|
-      formatted_date = table.by_col[1][3..-1][index].split("/")
-      date = Date.new(
-        formatted_date[2].to_i,
-        formatted_date[0].to_i,
-        formatted_date[1].to_i
-      )
-      month = date.strftime("%b")
-      entry = { "#{month}": {"#{description}": table.by_col[5][3..-1][index]} }
-      history.push(entry)
+    # entry_index = 0
+    # date_index = 0
+    # desc_index = 2
+    # amount_index = 5
+
+    data = extracted_table_row_data(
+      table: table[0..-1],
+      date_index: 0,
+      desc_index: 2,
+      amount_index: 5
+    )
+  end
+
+  expense_summary.each do |month|
+    monthly_expenses = data.select {|entry| entry.key?(month.keys[0])}
+    monthly_expenses.each do |expense|
+      month[month.keys[0]].push(expense[month.keys[0]])
     end
   end
 end
 
-months_reported = []
-
-history.each do |item|
-  months_reported.push(item.keys[0])
-end
-
-thing = {}
-
-months_reported.uniq.each do |month|
-  thing["#{month}"] = []
-end
-
-history.each do |item|
-  month = item.keys[0]
-  thing["#{month}"].push(item)
-end
-
-history = []
-history.push(thing)
-
-pp history
+pp expense_summary
+# V2 ---------
